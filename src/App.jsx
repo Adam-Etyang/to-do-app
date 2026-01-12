b9959558e307377dec121a9f6d38798d99a79a5c
Adam-Etyang <adametyang69@icloud.com>
2026-01-12 16:09:36 +0300

    Minimal get started page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
 import './App.css';
+import { Box, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 
 function App() {
+  const [empty, setEmpty] = useState(true);
+  if (empty) {
+    return (
+      <>
 
+        <Box position="relative" h="100vh" className='root' border='none'>
+          <div className='primary-conatainer'>
+            <div className='default'>
+              <img src="/empty-box.svg" alt="Empty box" className='box-image' />
+            </div>
+            <div className='secondary-text-box'>
+              <p className='secondary-text'>
+                This is you inbox it is where you put your tasks to accomplish
+              </p>
+            </div>
+            <div className='Add-task-button'>
+              <Button
+                onClick={() => { addTask() }}
+                size='sm'
+                colorPalette='yellow'>
+                Click ts
+              </Button>
+            </div>
+          </div>
+        </Box>
+
+
+      </>
+    );
+  }
   return (
     <>
-      <div className='container'>
-        <p>
-          This is a container</p>
-        <p>
-          This is a container</p>
-
-
-      </div>
-
     </>
   )
+
+
 }
 
 export default App
