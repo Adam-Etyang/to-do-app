93e3ead2d7f87323d00a815b72b6e18b2fde8695
Adam-Etyang <adametyang69@icloud.com>
2026-01-12 13:54:56 +0300

    project init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <p>
+          This is a container</p>
+        <p>
+          This is a container</p>
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
